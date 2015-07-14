5f46ad7bccedd671bd18fce36a094e2c4c23b047
Harry Sadler <harrysadlermusic@gmail.com>
2015-07-13 21:33:55 -0700

    complete specs

--- simulatedAnnealing.js
+++ simulatedAnnealing.js
@@ -6,7 +6,22 @@ var items = [ // allowed multiple of each
   {name:'radio',    weight:6, value:30}
 ];
 
+var weighItems = function(currSolution) {
+  var weight = 0;
+  for(var i = 0; i < currSolution.length; i++) {
+    weight += currSolution[i].weight;
+  }
+  return weight;
+}
+
 function generateRandomSolution(){
+  var solution = [];
+
+  while(weighItems(solution) < 18) {
+    solution.push(items[randomIndex(items)]);
+  }
+  solution.pop();
+
 
   return solution; // array of items, must be <= maxWeight
 };
@@ -14,10 +29,31 @@ function generateRandomSolution(){
 function generateNeighboringSolution(oldSolution){
   // add, swap, or remove item randomly
 
-  return solution; // array of items, must be <= maxWeight
+  //take a existing solution array
+  var oldSol = oldSolution.slice();
+
+  //pick an item at random to remove
+  var randI = randomIndex(oldSol);
+
+  oldSol.splice(randI, 1);
+
+  //add another random item if diff
+  while(weighItems(oldSol) < 18) {
+    oldSol.push(items[randomIndex(items)]);
+  }
+  oldSol.pop();
+  console.log(oldSol);
+
+  return oldSol; // array of items, must be <= maxWeight
 }
 
 function calculateCost(solution){
+
+  var cost = 0;
+
+  for(var i = 0; i < solution.length; i++) {
+    cost += solution[i].value;
+  }
 
   return cost; // sum of values of items
 }
@@ -27,6 +63,25 @@ function acceptance_probability(old_cost, new_cost, temperature){
 }
 
 function simulateAnnealing(){
+  var counter = 0;
+  //get initial random solution
+  var solution = generateRandomSolution(items);
+  //get neighbor solution and compare
+  var neighbor;
+  //if better, keep
+
+  while(counter < 100) {
+    neighbor = generateNeighboringSolution(solution);
+
+    if(calculateCost(solution) < calculateCost(neighbor)) {
+      solution = neighbor;
+    }
+    counter++;
+  }
+  // while(calculateCost(solution) < 140) {
+  //   anneal();
+  // }
+  // //if not better, run again
 
   return solution; // array of items, must be <= maxWeight
 };
